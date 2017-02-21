41a17be0a4249e89b02c36f04889653f9708c25d
Vipul P. Ujawane <vipul999ujawane@gmail.com>
2017-02-22 01:58:01 +0530

    Homepage Created
    homepage created, urls set, views created

--- stud_search/urls.py
+++ stud_search/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from students import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',views.home),
 ]
