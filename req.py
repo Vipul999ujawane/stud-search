130d23eda0c55b014a655e19bec7af579d5fce8d
Vipul Ujawane <vipul999ujawane@gamil.com>
2017-02-25 01:03:17 +0530

    Database Created

--- req.py
+++ req.py
@@ -23,9 +23,19 @@ for row in data[1:]:
     dictt={}
     dictt=dict(roll=row[0],name=row[1],hall=row[2],room=row[3],stat=row[4])
     result.append(dictt)
-
-with open('data.json','w') as out:
-    json.dump(result,out)
+r2=[]
+i=0;
+for row in result:
+    dictt={}
+    dictt["model"]="students.stud"
+    dictt["pk"]=i+1;
+    dictt["fields"]=result[i]
+    r2.append(dictt)
+    i+=1
+    
 pp=pprint.PrettyPrinter(indent=3)
-pp.pprint(json.dumps(result))
+#pp.pprint(r2)
+pp.pprint(json.dumps(r2))
+with open("data.json","w") as out:
+    json.dump(r2,out)
 print("Parsing Successful")
--- students/models.py
+++ students/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Stud(models.Model):
+    roll=models.CharField(max_length=10)
+    name=models.CharField(max_length=100)
+    hall=models.CharField(max_length=10)
+    room=models.CharField(max_length=10)
+    stat=models.CharField(max_length=100)
