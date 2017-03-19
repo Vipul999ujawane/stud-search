1e99bf877a7db7d77e7fd7c6b84a5d5991e05388
Vipul999ujawane <vipul999ujawane@gmail.com>
2017-03-19 22:27:51 +0530

    Added Pagination

--- students/views.py
+++ students/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from students.models import Stud
+from django.core.paginator import Paginator
 # Create your views here.
 def home(request):
     return render(request,'home.html')
 def search(request):
     if 'q' in request.GET:
         q=request.GET['q']
+        if 'page' in request.GET:
+            page=request.GET['page']
+        else:
+            page =1
+        
         students=Stud.objects.filter(name__icontains=q) or Stud.objects.filter(roll__icontains=q)
+        paginator=Paginator(students,10)
+
+        try:
+            students= paginator.page(page)
+        except PageNotAnInteger:
+            students=paginator.page(1)
+        except EmptyPage:
+            students=paginator.page(paginator.num_pages)
+            
         return render(request,'search.html',{'students':students})
